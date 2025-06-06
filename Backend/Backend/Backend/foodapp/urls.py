9f6d99bdd7e6cdd3f88f7ecefd431b97a2a0eced
AliDarvishi <darvishiali340@gmail.com>
2025-06-06 18:17:13 +0330

    Delete Backend directory

--- Backend/Backend/Backend/foodapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'foodapp'
-
-urlpatterns = [
-    path('foods/', views.food_list, name='food_list'),
-    path('foods/<int:food_id>/', views.food_detail, name='food_detail'),
-] 
