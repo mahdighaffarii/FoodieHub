7d11469a028ff8c9ec01d53a307b0362de88d6d9
Mahdi <mahdi2015gh@gmail.com>
2025-06-12 01:47:21 +0330

    Create view for handling new orders

--- Backend/orders/views.py
+++ Backend/orders/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Order
+from .serializers import OrderCreateSerializer
 
-# Create your views here.
+class CreateOrderView(generics.CreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderCreateSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        # ارسال کاربر فعلی برای استفاده در serializer
+        serializer.save()
