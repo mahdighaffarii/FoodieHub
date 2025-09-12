d6d27fb4fe1332e78005c0f31442494d849c21f7
AliDarvishi <darvishiali340@gmail.com>
2025-09-12 22:04:32 +0330

    Delete src directory

--- src/api/auth.api.ts
+++
@@ -1,8 +0,0 @@
-import http from './http';
-
-export const login = (email: string, password: string) =>
-  http.post('/api/accounts/login/', { email, password });
-
-export const refresh = (refresh: string) =>
-  http.post('/api/accounts/login/refresh/', { refresh });
-                
